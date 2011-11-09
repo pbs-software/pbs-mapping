d75e1e60378027951217712d77982e821d0d1cc5
rowan.haigh <haighr@users.noreply.github.com>
2011-11-09 17:42:13 +0000

    namespace requires the use of '.onLoad' instead of '.First.lib'.

--- PBSmapping/R/zzz.r
+++ PBSmapping/R/zzz.r
@@ -1,20 +1,22 @@
-.First.lib <- function(lib, pkg)
+.onLoad <- function(lib, pkg)
 {
 	library.dynam("PBSmapping", pkg, lib);
-	pkg_info <- utils::sessionInfo( package=pkg )$otherPkgs[[ pkg ]]
-	pkg_date <- strsplit( pkg_info$Packaged, " " )[[1]][1]
+	pkg_info <- utils::sessionInfo( package="PBSmapping" )$otherPkgs$PBSmapping
+	if( is.character( pkg_info$Packaged ) )
+		pkg_date <- strsplit( pkg_info$Packaged, " " )[[1]][1]
+	else
+		pkg_date  <- "unkown"
 	
-	userguide_path <- system.file( "doc/PBSmapping-UG.pdf", package = pkg )
+	userguide_path <- system.file( "doc/PBSmapping-UG.pdf", package = "PBSmapping")
 	
 	cat("
-PBS Mapping", pkg_info$Version, "-- Copyright (C) 2003-2010 Fisheries and Oceans Canada
-
 -----------------------------------------------------------
+PBS Mapping", pkg_info$Version, "-- Copyright (C) 2003-2011 Fisheries and Oceans Canada
+
 PBS Mapping comes with ABSOLUTELY NO WARRANTY;
 for details see the file COPYING.
 This is free software, and you are welcome to redistribute
 it under certain conditions, as outlined in the above file.
------------------------------------------------------------
 
 A complete user guide 'PBSmapping-UG.pdf' is located at 
 ", userguide_path, "
@@ -23,6 +25,8 @@ To see demos, type '.PBSfigs()'.
 
 Packaged on", pkg_date, "
 Pacific Biological Station, Nanaimo
+-----------------------------------------------------------
+
 
 ")
 }
