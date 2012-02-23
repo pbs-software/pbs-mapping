7b4bb694d33939bfd838a28d853a0755262517c7
boersn@macewan.ca <boersn@macewan.ca@bf84610c-6202-11de-85c0-7b9747e48d0e>
2012-02-23 16:56:21 +0000

    Improve long/lat <=> UTM conversions; update documentation (Issue #9)
    Issue #9 noted erroneous results given data that spanned more than one
    zone to the right/left of the intended central zone.  This update
    improves our implementation by correcting a number of (implementation)
    errors (described below), but the problem still persists.
    
    The earlier (2001) implementation had a number of optimizations that
    made it terribly difficult to verify the code against the published
    algorithm and inspecting the implementation revealed some errors:
    (1) the calculation of 'n' used scaled 'a'/'b' rather than original
        'a'/'b',
    (2) the calculation of 'e^2' used scaled 'a'/'b' rather than original
        'a'/'b', and
    (3) the calculation of 'm' was not iterative as suggested in the
        algorithm.
    This new version more closely matches the published algorithm and is
    consequently easy to verify.  The performance is similar to the old
    algorithm (faster in some cases, slower in others).

--- trunk/PBSmapping/src/conversions.c
+++ trunk/PBSmapping/src/conversions.c
@@ -44,190 +44,177 @@
     17 Jun 2004 [Nicholas Boers]
       - cleaned up comments
   
-  Algorithm source:
-    National Mapping Agency of Great Britain Ordnance Survey
-    <http://www.ordsvy.gov.uk>
+    For further history, see repository log.
+
+  References:
+
+    [Ord10] Ordnance Survey. 2010. A guide to coordinate systems
+      in Great Britain. Report D00659 (v2.1). Southampton, UK.
+      http://www.ordnancesurvey.co.uk/oswebsite/gps/docs/
+           A_Guide_to_Coordinate_Systems_in_Great_Britain.pdf
+
   ---------------------------------------------------------------------------*/
 
 #include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
 #include <string.h>
+
 #include "globals.h"
 #include "conversions.h"
 
 /* constants of the earth (Ellipsoidal) and conversion factors */
-#define A_PRE   6378137.0                 /* major axis of ellipsoid */
-#define B_PRE   6356752.3141              /* minor axis of ellipsoid */
-#define SF      0.9996                    /* scale factor */
-#define A       (A_PRE * SF)              /* major after scaled */
-#define B       (B_PRE * SF)              /* minor after scaled */
-#define N       ((A - B) / (A + B))       /* ratio used in calculations */
-#define E       sqrt(((A*A)-(B*B))/(A*A)) /* eccentricity */
-#define E0      500000.0                  /* grid eastings of true origin */
+#define _a	6378137.0000		/* major axis of ellipsoid */
+#define _b	6356752.3141		/* minor axis of ellipsoid */
+#define _e2	(((_a*_a) - (_b*_b)) / (_a*_a)) /* eccentricity squared; B1 */
+#define _n	((_a - _b) / (_a + _b))	/* C1 */
+#define _n2	(_n * _n)		/* _n^2 */
+#define _n3	(_n2 * _n)		/* _n^3 */
+#define _phi0	0.0			/* true origin, latitude */
+/*      lambda0 (-177+((utmZone-1)*6.0))   true origin, longitude */
+#define _N0	0.0			/* true origin, northings (metres) */
+#define _E0	500000.0                /* true origin, eastings (meters) */
+#define _F0	0.9996			/* scale factor */
 
-#ifndef FALSE
-#define FALSE 0
+/* getNu: get radius of curvature at lat. 'phi' perpendicular to a meridian */
+#define getNu(phi) (_a * _F0 * pow(1 - _e2 * pow(sin(phi), 2), -0.5))
+/* getRho: get a radius of curvature of a meridian at latitude 'phi' */
+#define getRho(nu,phi) (_a * _F0 * (1 - _e2) * pow(1 - _e2		\
+						   * pow(sin(phi), 2), -1.5))
+/* getEta2 */
+#define getEta2(nu,rho) ((nu / rho) - 1.0)
+/* getM: get developed arc of a meridian from 'phi' to zero */
+#define getM(phi) ( (_b * _F0)						\
+		    * ( ( (1 + _n + 5.0/4.0*_n2 + 5.0/4.0*_n3)		\
+			  * (phi - _phi0) )				\
+			- ( (3*_n + 3*_n2 + 21.0/8.0*_n3)		\
+			    * (sin(phi - _phi0) * cos(phi + _phi0)) )	\
+			+ ( (15.0/8.0*_n2 + 15.0/8.0*_n3)		\
+			    * (sin(2*(phi - _phi0)) * cos(2*(phi + _phi0))) ) \
+			- ( (35.0/24.0*_n3)				\
+			    * (sin(3*(phi - _phi0)) * cos(3*(phi + _phi0))) ) ) )
+
+/*------------------------------------------------------------------------------
+ * lonlat_to_utm:
+ *   Convert coordinates in longitude (lambda)/latitude (phi) to
+ *   eastings/northings.
+ * 
+ * Source: 
+ *   [Ord10, Appendix C, Section C.1]
+ *
+ * See note for utm_to_lonlat.
+ *----------------------------------------------------------------------------*/
+void lonlat_to_utm(double lambda, double phi, struct pair *utm, int utmZone)
+{
+#if !defined(_N0) || !defined(_E0) || !defined(_F0) || !defined(_phi0)
+#error "One or more of the required constants is undefined."
 #endif
 
-#ifndef TRUE
-#define TRUE 1
+    double lambda0 = (-177 + ((utmZone - 1) * 6.0)) * DEG_TO_RAD;
+
+    double nu, rho, eta2;
+    nu = getNu (phi);
+    rho = getRho (nu, phi);
+    eta2 = getEta2 (nu, rho);					/* C2 */
+
+    double M;
+    M = getM (phi);						/* C3 */
+
+    double I, II, III, IIIA, IV, V, VI;
+    double cosphi = cos(phi), tanphi = tan(phi), sinphi=sin(phi);
+    I = M + _N0;
+    II = (nu / 2.0) * sinphi * cosphi;
+    III = ( (nu / 24.0) * sinphi * pow(cosphi, 3)
+	    * (5.0 - pow(tanphi, 2) + 9.0 * eta2) );
+    IIIA = ( (nu / 720.0) * sinphi * pow(cosphi, 5)
+	     * (61.0 - 58.0 * pow(tanphi, 2) + pow(tanphi, 4)) );
+    IV = nu * cosphi;
+    V = ( (nu / 6.0) * pow(cosphi, 3) * ((nu / rho) - pow(tanphi, 2)) );
+    VI = ( (nu / 120.0) * pow(cosphi, 5)
+	   * (5.0 - 18.0 * pow(tanphi, 2)
+	      + pow(tanphi, 4) + 14.0 * eta2 
+	      - 58.0 * pow(tanphi, 2) * eta2) );
+    
+    utm->y = I + II * pow(lambda - lambda0, 2)
+	+ III * pow(lambda - lambda0, 4)
+	+ IIIA * pow(lambda - lambda0, 6);			/* C4 */
+    utm->x = _E0 + IV * (lambda - lambda0)
+	+ V * pow(lambda - lambda0, 3)
+	+ VI * pow(lambda - lambda0, 5);			/* C5 */
+}
+
+/*------------------------------------------------------------------------------
+ * utm_to_lonlat:
+ *
+ * Source: 
+ *   [Ord10, Appendix C, Section C.2]
+ *
+ * Note:
+ * For clarity, this (2012) version doesn't attempt to reduce calls to
+ * trigonometric functions as much as previously -- and we haven't
+ * noticed a significant performance hit by removing many of the
+ * earlier "optimizations."
+ *----------------------------------------------------------------------------*/
+void utm_to_lonlat(double E, double N, struct pair *lonlat,
+                   int utmZone)
+{
+#if !defined(_a) || !defined(_b) || !defined(_e2) || !defined(_N0) || \
+    !defined(_F0) || !defined(_phi0)
+#error "One or more of the required constants is undefined."
 #endif
 
-/*-----------------------------------------------------------------------------
-  get_nu:
-   
-  Description:
-  - get a radius of curvature at latitude 'phi' perpendicular to a meridian
-  ---------------------------------------------------------------------------*/
-static double get_nu(double phi)
-{
-  double sin1, sin2;
+    /* calculate M and adjust phiPrime in the process */
+    double phiPrime, M;
+    phiPrime = ((N - _N0) / (_a * _F0)) + _phi0;		/* C6 */
+ redo:
+    M = getM (phiPrime);					/* C3 */
+    /* 0.00001 m = 0.01 mm */
+    if (fabs(N - _N0 - M) >= 0.00001) {
+	phiPrime += ((N - _N0 - M) / (_a * _F0));		/* C7 */
+	goto redo;
+    }
 
-  sin1 = sin(phi);
-  sin2 = sin1 * sin1;
+    /* (N - _N0 - M) < 0.00001 */
+    double nu, rho, eta2;
+    nu = getNu (phiPrime);
+    rho = getRho (nu, phiPrime);
+    eta2 = getEta2 (nu, rho);					/* C2 */
 
-  return (A / (sqrt(1.0 - (E * E * sin2))));
-}
-
-/*-----------------------------------------------------------------------------
-  get_rho:
-   
-  Description:
-  - get a radius of curvature of a meridian at latitude 'phi'
-  ---------------------------------------------------------------------------*/
-static double get_rho(double nu, double phi)
-{
-  double sin1, sin2;
-
-  sin1 = sin(phi);
-  sin2 = sin1 * sin1;
-
-  return ((nu * (1.0 - E * E)) / (1.0 - (E * E * sin2)));
-}
-
-/*-----------------------------------------------------------------------------
-  get_eta:
-  ---------------------------------------------------------------------------*/
-static double get_eta(double nu, double rho)
-{
-  return (sqrt((nu / rho) - 1.0));
-}
-
-/*-----------------------------------------------------------------------------
-  get_M:
-
-  Description:
-  - get developed arc of a meridian from 'phi' to zero
-  ---------------------------------------------------------------------------*/
-static double get_M(double phi)
-{
-  double c1, c2, c3, c4, n2, n3, sin1, cos1, M;
-
-  c1 = 5.0/4.0;
-  c2 = 21.0/8.0;
-  c3 = 15.0/8.0;
-  c4 = 35.0/24.0;
-  n2 = N*N;
-  n3 = n2*N;
-  sin1 = sin(phi);
-  cos1 = cos(phi);
-  M =     ((1.0 + N + (c1*n2) + (c1*n3)) * phi);
-  M = M - (((3.0*N) + (3.0*n2) + (c2*n3)) * sin(phi) * cos(phi));
-  M = M + (((c3*n2) + (c3*n3)) * sin(2.0*phi) * cos(2.0*phi));
-  M = M - ((c4*n3) * sin(3.0*phi) * cos(3.0*phi));
-
-  return (B*M);
-}
-
-/*-----------------------------------------------------------------------------
-  lonlat_to_utm:
-  ---------------------------------------------------------------------------*/
-void lonlat_to_utm(double lon, double lat, struct pair *eastingNorthing,
-                   int utmZone)
-{
-  double sin1, cos1, cos3, cos5, tan1, tan2, tan4, nu, rho, eta, eta2;
-  double P, P2, P3, P4, P5, P6, I, II, III, IIIA, IV, V, VI;
-  int lonOrig = -177 + ((utmZone - 1) * 6);
-
-  sin1 = sin(lat);
-  cos1 = cos(lat);
-  cos3 = cos1*cos1*cos1;
-  cos5 = cos3*cos1*cos1;
-  tan1 = tan(lat);
-  tan2 = tan1*tan1;
-  tan4 = tan2*tan2;
-  nu = get_nu(lat);
-  rho = get_rho(nu,lat);
-  eta = get_eta(nu,rho);
-  eta2 = eta*eta;
-  P = lon - (lonOrig * DEG_TO_RAD);
-  P2 = P*P;
-  P3 = P2*P;
-  P4 = P3*P;
-  P5 = P4*P;
-  P6 = P5*P;
-  I = get_M(lat);
-  II = (nu/2.0)*sin1*cos1;
-  III = (nu/24.0)*sin1*cos3*(5.0-tan2+9.0*eta2);
-  IIIA = (nu/720.0)*sin1*cos5*(61.0-58.0*tan2+tan4);
-  IV = nu*cos1;
-  V = (nu/6.0)*cos3*((nu/rho)-tan2);
-  VI = (nu/120.0)*cos5*(5.0-18.0*tan2+tan4+14.0*eta2-58.0*tan2*eta2);
-
-  eastingNorthing->y = I+P2*II+P4*III+P6*IIIA;
-  eastingNorthing->x = E0+P*IV+P3*V+P5*VI;
-}
-
-/*-----------------------------------------------------------------------------
-  utm_to_lonlat:
-  ---------------------------------------------------------------------------*/
-void utm_to_lonlat(double easting, double northing, struct pair *lonlat,
-                   int utmZone)
-{
-  double E1, E2, E3, E4, E5, E6, E7, lat1, M, nu, rho, eta, eta2;
-  double tan1, tan2, tan4, tan6, sec1, nu3, nu5, nu7, VII, VIII;
-  double IX, X, XI, XII, XIIA;
-  int lonOrig = -177 + ((utmZone - 1) * 6);
-  int i;
-
-  E1 = easting - E0;
-  E2 = E1*E1;
-  E3 = E2*E1;
-  E4 = E3*E1;
-  E5 = E4*E1;
-  E6 = E5*E1;
-  E7 = E6*E1;
-  lat1 = northing/A;
-  M = get_M(lat1);
-
-  for(i=0;i<10;i++){
-    lat1 += (northing-M)/A;
-    M = get_M(lat1);
-  }
-
-  nu = get_nu(lat1);
-  rho = get_rho(nu,lat1);
-  eta = get_eta(nu,rho);
-  eta2 = eta*eta;
-  tan1 = tan(lat1);
-  tan2 = tan1*tan1;
-  tan4 = tan2*tan2;
-  tan6 = tan4*tan4;
-  sec1 = 1.0/(cos(lat1));
-  nu3 = nu*nu*nu;
-  nu5 = nu3*nu*nu;
-  nu7 = nu5*nu*nu;
-  VII = tan1/(2.0*rho*nu);
-  VIII = tan1/(24.0*rho*nu3)*(5.0+3.0*tan2+eta2-9.0*tan2*eta2);
-  IX = tan1/(720.0*rho*nu5)*(61.0+90.0*tan2+45.0*tan4);
-  X = sec1/nu;
-  XI = sec1/(6.0*nu3)*(nu/rho+2.0*tan2);
-  XII = sec1/(120.0*nu5)*(5.0+28.0*tan2+24.0*tan4);
-  XIIA = sec1/(5040.0*nu7)*(61.0+662.0*tan2+1320.0*tan4+720.0*tan6);
-
-  /* 11 Jul 2003 [Nicholas Boers]
-     Swapped the x and y, because values being returned were backwards. */
-  lonlat->x = E1*X-E3*XI+E5*XII-E7*XIIA + (lonOrig * DEG_TO_RAD);
-  lonlat->y = lat1 - E2*VII+E4*VIII-E6*IX;
+    double VII, VIII, IX, X, XI, XII, XIIA;
+    double nu2=nu*nu, nu3=nu2*nu, nu4=nu2*nu2, nu5=nu4*nu, nu6=nu4*nu2,
+	nu7=nu6*nu;
+    VII = tan(phiPrime) / (2.0 * rho * nu);
+    VIII = ( (tan(phiPrime) / (24.0 * rho * nu3))
+	     * (5.0 + 3.0 * pow(tan(phiPrime), 2)
+		+ eta2
+		- 9 * pow(tan(phiPrime), 2) * eta2));
+    IX = ( (tan(phiPrime) / (720.0 * rho * nu5))
+	   * (61.0
+	      + 90.0 * pow(tan(phiPrime), 2)
+	      + 45.0 * pow(tan(phiPrime), 4)) );
+    X = (1.0 / cos(phiPrime)) / nu;
+    double secPhiPrime = (1.0 / cos(phiPrime));
+    XI = ( (secPhiPrime / (6.0 * nu3))
+	   * ((nu / rho)
+	      + 2.0 * pow(tan(phiPrime), 2)) );
+    XII = ( (secPhiPrime / (120.0 * nu5))
+	    * (5.0
+	       + 28.0 * pow(tan(phiPrime), 2)
+	       + 24.0 * pow(tan(phiPrime), 4)) );
+    XIIA = ( (secPhiPrime / (5040.0 * nu7))
+	     * (61.0
+		+ 662.0 * pow(tan(phiPrime), 2)
+		+ 1320.0 * pow(tan(phiPrime), 4)
+		+ 720.0 * pow(tan(phiPrime), 6)) );
+    
+    double lambda0 = (-177 + ((utmZone - 1) * 6.0)) * DEG_TO_RAD;
+    lonlat->y = ( phiPrime
+		  - VII * pow(E - _E0, 2)
+		  + VIII * pow(E - _E0, 4)
+		  - IX * pow(E - _E0, 6) );			/* C8 */
+    lonlat->x = ( lambda0
+		  + X * (E - _E0)
+		  - XI * pow(E - _E0, 3)
+		  + XII * pow(E - _E0, 5)
+		  - XIIA * pow(E - _E0, 7) );			/* C9 */
 }
