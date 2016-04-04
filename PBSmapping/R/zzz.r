4333e2c8179ef48db451325d74ac4d2c0e5afee8
haighr <rowan.haigh@dfo-mpo.gc.ca>
2016-04-04 10:02:37 -0700

    2.70.1 (2016-04-04)   • Added a new function `addCompass' that can add a compass rose to a map. (RH 160401)   • Added a new function `calcGCdist' to calculate great-circle distance. (RH 160401)   • Updated `PBSmappingIntro.Rnw' (RH 160331)

--- PBSmapping/R/zzz.r
+++ PBSmapping/R/zzz.r
@@ -35,7 +35,7 @@ Packaged on ", pkg_date, "
 Pacific Biological Station, Nanaimo
 
 All available PBS packages can be found at
-http://code.google.com/p/pbs-software/
+https://github.com/pbs-software
 
 To see demos, type '.PBSfigs()'.
 -----------------------------------------------------------
